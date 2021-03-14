e83e4d0465fb7f6776a025632018a765d17d7824
Fabian Brune <fabian.brune@yahoo.de>
2021-03-14 16:12:45 +0100

    implemented addBall and calculateScore in Line class

--- src/main/java/Line.java
+++ src/main/java/Line.java
@@ -1,7 +1,6 @@
 import java.util.ArrayList;
 
 public class Line {
-    public void addBall(int ballValue) {
 
     private ArrayList<Frame> frames = new ArrayList();
 
@@ -12,9 +11,40 @@ public class Line {
     private Frame getLastFrame(){
         return frames.get(frames.size()-1);
     }
+
+    public void addBall(int ballPins) {
+        // add ball to last frame
+        getLastFrame().addBall(ballPins);
+
+        // if the last frame is now finished (2 balls or strike)
+        if(getLastFrame().isFinished()) {
+            // add a new empty frame
+            Frame newFrame = new Frame();
+            frames.add(newFrame);
+            // link previous frames to the new one
+            int indexOfLastFrame = frames.size()-1;
+            System.out.println("frames.size() is now "+indexOfLastFrame);
+            if (indexOfLastFrame >= 1) {
+                System.out.println("linking 1");
+                frames.get(indexOfLastFrame - 1).setNextFrame(newFrame);
+            }
+            if (indexOfLastFrame >= 2) {
+                System.out.println("linking 2");
+                frames.get(indexOfLastFrame - 2).setSecondNextFrame(newFrame);
+            }
+        }
     }
 
     public int calculateScore() {
-        return 0;
+        // calculate score
+        int score = 0;
+        System.out.println("score is now "+score);
+        int i = 0;
+        while(i < frames.size() && i < 10){
+            score += frames.get(i).calculateScore();
+            System.out.println("score is now "+score);
+            i++;
+        }
+        return score;
     }
 }
