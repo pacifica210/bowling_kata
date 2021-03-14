557870cbda7301a1b91109358cc00bbebcd6a8d5
Fabian Brune <fabian.brune@yahoo.de>
2021-03-14 15:42:20 +0100

    added frames list

--- src/main/java/Line.java
+++ src/main/java/Line.java
@@ -1,6 +1,13 @@
+import java.util.ArrayList;
+
 public class Line {
     public void addBall(int ballValue) {
 
+    private ArrayList<Frame> frames = new ArrayList();
+
+    private Frame getLastFrame(){
+        return frames.get(frames.size()-1);
+    }
     }
 
     public int calculateScore() {
