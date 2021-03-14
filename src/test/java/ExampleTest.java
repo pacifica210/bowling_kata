3630c2ab9d5c23a8e8d7fb25892913bb3511c21b
Fabian Brune <fabian.brune@yahoo.de>
2021-03-14 10:48:30 +0100

    renamed example, added DisplayName

--- src/test/java/ExampleTest.java
+++ src/test/java/ExampleTest.java
@@ -1,13 +1,16 @@
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ExampleTest {
     @Test
-    void myTest() {
+    @DisplayName("example test method")
+    void exampleTestMethod() {
         int a = 1, b = 42;
-        Assertions.assertAll(
+        assertAll(
                 () -> assertEquals(a, 1),
                 () -> assertEquals(b, 42));
     }
