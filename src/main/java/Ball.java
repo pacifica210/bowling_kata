c8fe036af156da28a4be4e077a849ce23c9424de
Fabian Brune <fabian.brune@yahoo.de>
2021-03-14 11:55:14 +0100

    added Ball tests, added generated function in class Ball

--- src/main/java/Ball.java
+++ src/main/java/Ball.java
@@ -1,2 +1,16 @@
 public class Ball {
+
+    private int pins;
+
+    public Ball(int pins) {
+        this.pins = pins;
+    }
+
+    public int getPins() {
+        return pins;
+    }
+
+    public void setPins(int pins) {
+        this.pins = pins;
+    }
 }
--- src/test/java/BallTest.java
+++ src/test/java/BallTest.java
@@ -1,2 +1,24 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class BallTest {
+    @Test
+    @DisplayName("basic ball test")
+    void basicBallTest() {
+        int[] testValues = {0, 1, 10, -1, 11};
+        Collection<Executable> listOfAsserts = new LinkedList<>();
+        for (int testValue : testValues){
+            listOfAsserts.add(()->assertEquals(testValue, (new Ball(testValue)).getPins()));
+        }
+        assertAll(listOfAsserts);
+    }
 }
